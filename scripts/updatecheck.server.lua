3987d5575e64a3a915e3279b7864165047624b16
JR10 <hamdyjs@gmail.com>
2015-03-12 21:34:10 +0200

    Change license to MIT

--- scripts/updatecheck.server.lua
+++ scripts/updatecheck.server.lua
@@ -1,19 +1,3 @@
--- Business System v1.2 - updatecheck.server.lua - Checks for any updates for the resource on the community
--- Copyright (C) 2012-2015, JR10
---
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 addEventHandler("onResourceStart", resourceRoot, function()
 	callRemote("http://community.mtasa.com/mta/resources.php", function(name, version)
 		if (name:lower():find("error") or version == 0) then return; end
